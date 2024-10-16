a2a357615dd9e916e985b466ebe7462838d14933
Maciej Szalewicz <m.szalewicz@gmail.com>
2024-10-16 16:08:11 +0200

    Create and bootstrap db if it does not exists

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,49 @@
 package main
 
 import (
+	"database/sql"
+	_ "embed"
+	"log"
 	"log/slog"
 	"os"
 
 	"gioui.org/app"
 	"gioui.org/unit"
 	"github.com/go-gl/glfw/v3.3/glfw"
+	"github.com/mszalewicz/skald/database"
 	"github.com/mszalewicz/skald/gui"
 	"github.com/mszalewicz/skald/network"
 )
 
+//go:embed schema.sql
+var schema string
+var backend database.Backend
+
 func init() {
+
+	{ // Create local db if it does not exist
+		dbFile, err := os.OpenFile("db.sqlite", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+		if err != nil {
+			slog.Error("Could not create log file.", "error", err)
+		}
+		defer dbFile.Close()
+	}
+
+	{ // Init database
+		db, err := sql.Open("sqlite3", "db.sqlite")
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		backend.DB = db
+
+		// Create tables
+		if _, err := backend.DB.Exec(schema); err != nil {
+			log.Fatal(err)
+		}
+	}
+
 	if err := glfw.Init(); err != nil {
 		panic(err)
 	}
@@ -50,7 +82,7 @@ func main() {
 		window.Option(app.Title("Skald"))
 		window.Option(app.Size(unit.Dp(width), unit.Dp(height)))
 		window.Option(app.MinSize(unit.Dp(minWidth), unit.Dp(minHeight)))
-		err := gui.MainWindow(window, &screen, &settings)
+		err := gui.MainWindow(window, &screen, &settings, &backend)
 
 		if err != nil {
 			slog.Error(err.Error())
