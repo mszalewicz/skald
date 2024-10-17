ab4568f7f88680aa4b87dadae4632f1988c79d90
Maciej Szalewicz <m.szalewicz@gmail.com>
2024-10-17 16:23:44 +0200

    DB schema

--- cmd/schema.sql
+++ cmd/schema.sql
@@ -1,6 +1,6 @@
-CREATE TABLE settings (
+CREATE TABLE IF NOT EXISTS settings (
     id        integer PRIMARY KEY,
-    width     integer    NOT NULL,
-    weight    integer    NOT NULL,
-    fontsize  integer
+    width     integer NOT NULL,
+    height    integer NOT NULL,
+    fontsize  integer NOT NULL
 ) strict;
