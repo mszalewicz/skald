7f7174dd187159c53a463ff6cc1bbc273d54a060
Maciej Szalewicz <m.szalewicz@gmail.com>
2024-10-24 17:39:34 +0200

    Added bool assert and no-op maybe to asserts

--- assert/assert.go
+++ assert/assert.go
@@ -6,10 +6,22 @@ import (
 	"runtime/debug"
 )
 
+func Assert(condition bool) {
+	if !condition {
+		fmt.Fprintf(os.Stderr, "ASSERT\n")
+		fmt.Fprintln(os.Stderr, string(debug.Stack()))
+		os.Exit(1)
+	}
+}
+
 func AssertNil(object any) {
 	if object == nil {
 		fmt.Fprintf(os.Stderr, "ASSERT\n")
 		fmt.Fprintln(os.Stderr, string(debug.Stack()))
 		os.Exit(1)
 	}
+}
+
+func Maybe(object any) {
+
 }
